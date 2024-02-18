3348a88786d3574cffe4dcb5f0d15d1cb00088d6
Anthony Cox <anthonyjohnc@gmail.com>
2024-02-18 13:59:22 +0000

    Update build to latest version

---
+++ build/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+require("core-js/modules/es.object.define-property.js");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+/**
+ * Developed by Anthony Cox in 2024
+ */
+var mainConfig = {
+  stories: ['../stories/**/*.stories.js'],
+  addons: ['@storybook/addon-a11y', '@storybook/addon-docs']
+};
+var _default = exports["default"] = mainConfig;
